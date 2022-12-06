1f05f45297537776d188f545ca6d4f925b93612b
Jay Prakash S <jpcricketer1998@gmail.com>
2022-12-06 14:44:00 +0530

    style updated

---
+++ app.js
@@ -0,0 +1,34 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var inputTxt = document.querySelector("#input-txt")
+var outputDiv = document.querySelector("#output")
+
+var serverURL =	"https://api.funtranslations.com/translate/cheunh.json"
+
+
+function translationURL(text)
+{
+  return serverURL + "?" + "text=" + text 
+}
+
+function errorHandler(error)
+{
+  console.log("error occured", error);
+  console.log("Something went wrong try again after some time")
+}
+
+function clickHandler()
+{
+  var txtInput = inputTxt.value;
+
+  fetch(translationURL(txtInput))
+  .then(response => response.json())
+  .then(json => {
+    var translatedText = (json.contents.translated)
+    outputDiv.innerText = translatedText;
+  })
+
+  .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickHandler)
+
